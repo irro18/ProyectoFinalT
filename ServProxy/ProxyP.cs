76b8000ecd1be5866f64c96c84f9d745966de4a9
Ivan <ivan18ro@gmail.com>
2019-12-03 10:14:00 -0600

    P

---
+++ ServProxy/ProxyP.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+
+namespace TwitterClone.ServProxy
+{
+    public class ProxyP : IProxyP
+    {
+        private RestClient _client;
+        private string appid = "";
+        private string metrics = "metric";
+
+        public ProxyP()
+        {
+            _client = new RestClient("https://restcountries.eu/rest/v2/all");
+        }
+
+
+        public List<CityObject> city()
+        {
+            var request = new RestRequest();
+            var response = _client.Get<List<CityObject>>(request);
+            return response.Data;
+        }
+    }
+}
+    
